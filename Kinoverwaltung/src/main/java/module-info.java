26ab95317ca23a564d4f5245c7bdeaf8132ef439
philippcroce <131445274+philippcroce@users.noreply.github.com>
2023-05-26 08:22:15 +0200

    Delete Kinoverwaltung directory

--- Kinoverwaltung/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.example.kinoverwaltung {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-
-    opens com.example.kinoverwaltung to javafx.fxml;
-    exports com.example.kinoverwaltung;
-}
