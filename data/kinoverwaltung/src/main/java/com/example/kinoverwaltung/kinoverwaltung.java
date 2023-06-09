81dd8e89a6e7763ef20cad5a79c6d9e16f8a4edb
philippcroce <philipp210506@gmail.com>
2023-06-09 21:02:16 +0200

    Update kinoverwaltung.java

--- data/kinoverwaltung/src/main/java/com/example/kinoverwaltung/kinoverwaltung.java
+++ data/kinoverwaltung/src/main/java/com/example/kinoverwaltung/kinoverwaltung.java
@@ -3,9 +3,14 @@ package com.example.kinoverwaltung;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
-
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -39,16 +44,21 @@ public class kinoverwaltung implements Initializable {
 
 
         try {
-            FXMLLoader loader = new FXMLLoader(getClass().getResource("buchen.fxml"));
-            Pane pane = loader.load();
-            Scene scene = new Scene(pane);
+            FXMLLoader loader = new FXMLLoader();
+            loader.setLocation(getClass().getResource("/com.example.kinoverwaltung/buchen.fxml"));
+            Parent root = loader.load();
+
             Stage stage = new Stage();
-            stage.setScene(scene);
-            stage.show();
-        } catch (Exception e) {
-            e.printStackTrace();
-                }
-            }
+
+            stage.setScene(new Scene(root));
+            stage.showAndWait();
+
+
+            //cbPersons.setItems(PersonDAO.getPersons());
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
 
 
     @Deprecated
