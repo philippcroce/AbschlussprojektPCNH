0c582ac56e1fbd151d8180477744bc056f0cdee7
philippcroce <philipp210506@gmail.com>
2023-06-11 19:11:59 +0200

    created mitarbeiter.fxml and some other changes

--- data/kinoverwaltung/src/main/java/com/example/kinoverwaltung/kinoverwaltung.java
+++ data/kinoverwaltung/src/main/java/com/example/kinoverwaltung/kinoverwaltung.java
@@ -3,31 +3,16 @@ package com.example.kinoverwaltung;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
-import javafx.scene.Parent;
 import javafx.scene.control.Label;
 import javafx.scene.control.Menu;
-import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
-import javafx.stage.Stage;
-import java.io.IOException;
-import java.net.URL;
-import java.util.ResourceBundle;
-import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.scene.layout.Pane;
-import javafx.stage.Stage;
 
 public class kinoverwaltung implements Initializable {
     @FXML
